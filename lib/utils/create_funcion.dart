29e497ba8b1fabbdfcdc861d0d9fa53749e71f16
shariful-islam-ssk <msharif330@gmail.com>
2026-03-06 15:27:06 +0600

    function practice motamuti completed

--- lib/utils/create_funcion.dart
+++ lib/utils/create_funcion.dart
@@ -1,14 +1,87 @@
-// int addNumbers({int a = 0, int b = 0}) {
-//   return a+b;
+// // int addNumbers({int a = 0, int b = 0}) {
+// //   return a+b;
+// // }
+// //
+// // void main() {
+// //   int result = addNumbers(a: 354, b: 556);
+// //
+// //   print('Sum: $result');
+// // }
+//
+// import 'package:flutter/cupertino.dart';
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// String addNumbers({required int a, required int b}) {
+//   int result =  a + b;
+//
+//   return '$result';
+// }
+//
+//
+//
+// String addnumbers({required int a, required int b}) {
+//   int result = a+b;
+//
+//   return '$result';
+// // }
+// //
+// String addNumbers ({required int a, required int b}) {
+//   int result= a+b;
+//
+//   return '$result';
 // }
 //
 // void main() {
-//   int result = addNumbers(a: 354, b: 556);
 //
-//   print('Sum: $result');
+//   String sum = addNumbers(a: 88, b: 851);
+//   print("sum of the addNumbers are: $sum");
+// }
+// //
+// //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// int multiplyNumbers(int a, int b) {
+//   return a * b;
+// }
+//
+//
+//
+// void main() {
+//   int multiply = multiplyNumbers(54, 64);
+//
+//   String sum = addNumbers(a: 54, b: 64);
+//
+//   debugPrint('sum: $sum multiply: $multiply ');
 // }
 
-import 'package:flutter/cupertino.dart';
 
 
 
@@ -26,10 +99,22 @@ import 'package:flutter/cupertino.dart';
 
 
 
-String addNumbers({required int a, required int b}) {
-  int result =  a + b;
 
-  return '$result';
+
+
+
+
+
+String addNumbers({required int a, required int b, }){
+  int modify=a+b;
+
+  return '$modify';
+}
+
+void main(){
+  String sum = addNumbers(a: 89, b: 44);
+
+  print("sum of the addnumbers are: $sum");
 }
 
 
@@ -46,25 +131,3 @@ String addNumbers({required int a, required int b}) {
 
 
 
-
-
-
-
-
-
-
-
-
-int multiplyNumbers(int a, int b) {
-  return a * b;
-}
-
-
-
-void main() {
-  int multiply = multiplyNumbers(54, 64);
-
-  String sum = addNumbers(a: 54, b: 64);
-
-  debugPrint('sum: $sum multiply: $multiply ');
-}
